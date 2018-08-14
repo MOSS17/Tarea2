0a7f253bea7c5f9a0d66088b897db6e0dabae9e7
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 10:05:17 -0700

    Agregar archivos de proyecto.

---
+++ Tarea2/Enemigo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Enemigo
+    {
+        public int posX;
+        public int posY;
+    }
+}
---
+++ Tarea2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo Enemigo1 = new Enemigo();
+        }
+    }
+}
